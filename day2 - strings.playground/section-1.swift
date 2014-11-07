a9e590e3c9ee1c49f4e28da987844b17ed317957
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-11-07 09:56:00 -0800

    Day 2: Strings

---
+++ day2 - strings.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+str = "hello, playground again"
+
+
+str + str + str
+
+var hello = "Hi there"
+var name = "Justin"
+
+hello + name
+
+hello + " " + name
+
+name = " Justin"
+
+hello + name
+
+hello + hello + hello + hello + hello
+
+"hello there, \(abc)"
+
+
+
