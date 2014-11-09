8c5f43f253731ea6dcf5cfe8875d7e8945341fe4
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-11-09 12:06:12 -0800

    day3_math

---
+++ Readme.md
@@ -0,0 +1,3 @@
+
+
+[Day 2: Strings](../../tree/a9e590e3c9ee1c49f4e28da987844b17ed317957)
---
+++ day3_math.playground/section-1.swift
@@ -0,0 +1,61 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var abc: Double = 10.0
+
+abc
+
+abc + 100
+abc * 20
+abc / 5
+abc / 3.0
+abc % 3
+
+abc = 9
+abc%3 == 0
+
+abc / (20*(40/22))
+
+var i = 0
+
+++i
+++abc
+--i
+i--
+i
+
+++i
+i
+i++
+i
+
+// abc = abc + 100
+abc += 100
+abc -= 100
+
+
+
+var de = 140.12
+
+floor(10.12)
+floor(de)
+// floor(1213) can't round an integer
+
+var be = 1213.0
+var to_round = be * 0.01
+floor(to_round) * 100
+
+
+
+
+
+
+
+
+
+
+
+
