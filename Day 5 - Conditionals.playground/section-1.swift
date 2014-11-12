1f10d2189bfb58578ef60dd6fe69e62460cf7960
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-11-11 17:18:21 -0800

    Day 5 - Conditionals.playground/timeline.xctimeline

--- Day 5 - Conditionals.playground/section-1.swift
+++ Day 5 - Conditionals.playground/section-1.swift
@@ -3,3 +3,62 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var userStatus: Bool = false
+
+userStatus == true
+userStatus == false
+
+
+str == "hello, playground"
+
+var temp = 85
+
+if temp < 80 {
+    println("it's cool out")
+} else {
+    println("it's warm")
+}
+
+temp <= 85
+temp == 90
+temp >= 32
+temp != 43
+temp >= 43 // cannot do temp !< 43
+
+!userStatus
+
+if userStatus {
+    println("user is logged in!")
+} else if !userStatus {
+    println("user is not logged in!")
+} else {
+    println("somethign went wrong")
+}
+
+
+
+if userStatus == true {
+    println("user is logged in!")
+} else if !userStatus {
+    println("user is not logged in!")
+} else {
+    println("somethign went wrong")
+}
+
+
+var userStringStatus = "Abc"
+
+if userStringStatus == "Some String" {
+    println("user is logged in!")
+} else if userStringStatus == "Abc" {
+    println("user is not logged in!")
+} else {
+    println("somethign went wrong")
+}
+
+
+
+
+
+
--- Day 6 - While Loops.playground/section-1.swift
+++ Day 6 - While Loops.playground/section-1.swift
@@ -3,3 +3,69 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var someTrueThing = true
+
+someTrueThing == true
+
+var someInt = 10
+
+while someInt < 20 {
+    // run some code
+    someInt++
+    // println("hello")
+}
+
+someInt
+var numLoops = 0
+
+runTheLoop: while someInt < 10 {
+    numLoops++
+    someInt += 3
+    // println(someInt)
+    switch numLoops {
+    case 0...100:
+        println(someInt)
+        if someInt % 2 == 0 {
+            println("\(someInt) is even")
+            continue runTheLoop
+        } else if someInt % 5 == 0 {
+            println("\(someInt) is divisible by 5")
+            // break runTheLoop
+        }
+    case 101...1000:
+        println("something else")
+    default:
+        println("this is the default")
+    }
+}
+
+
+var abc = 3
+
+while abc < 1000000000 {
+    println("\(abc)")
+    abc = abc * abc * abc
+}
+
+
+
+
+var theList = ["abc", "Justin", "jack", "john", "abby", "jill"]
+
+theList.count
+
+while theList.count < 10 {
+    theList.append("Another Name")
+}
+
+theList
+
+
+println(theList[0..<4])
+println(theList[0...3])
+
+
+
+
+
