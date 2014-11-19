6ad3b56b6d445ee1e9e0222f19d1f6d21dcda7da
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-11-18 16:11:59 -0800

    Day 14 - UI Table View

---
+++ Day14/Day14/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day14
+//
+//  Created by Justin Mitchel on 11/18/14.
+//  Copyright (c) 2014 Coding For Entrepreneurs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
