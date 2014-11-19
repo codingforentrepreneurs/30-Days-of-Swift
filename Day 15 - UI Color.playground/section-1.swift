6397a72c6dc103d1b9b7213cd73fb5aafeb218a6
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-11-18 16:48:41 -0800

    15 - UI Color

---
+++ Day 15 - UI Color.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+UIColor.redColor()
+UIColor.greenColor()
+UIColor.blackColor()
+UIColor.brownColor()
+UIColor.whiteColor()
+UIColor.grayColor()
+UIColor.blueColor()
+
+// UIColor(red: <#CGFloat#>, green: <#CGFloat#>, blue: <#CGFloat#>, alpha: <#CGFloat#>)
+
+UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+
+// rgba(0, 66, 140, 0.97) from Chrome's inspect element -- basic CSS colors
+
+
+
+UIColor(red: 0.0, green: 66.0, blue: 140.0, alpha: 1.0)
+
+UIColor(red: CGFloat(0.0/255.0), green: CGFloat(66.0/255.0), blue: CGFloat(140.0/255.0), alpha: 1.0)
